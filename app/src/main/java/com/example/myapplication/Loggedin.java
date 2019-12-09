b209b86e677269dfc55b63d3ab8d67edd984d087
Himanshu Anuragi <4rr0w.v+github@gmail.com>
2019-12-09 13:46:52 +0530

    logout added

--- app/src/main/java/com/example/myapplication/Loggedin.java
+++ app/src/main/java/com/example/myapplication/Loggedin.java
@@ -2,13 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Loggedin extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loggedin);
+        firebaseAuth = FirebaseAuth.getInstance();
+        logOut = findViewById(R.id.logout);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                Intent intent = new Intent(Loggedin.this,MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
     }
 }
