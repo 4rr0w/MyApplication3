931dcb97004b4b3ed29f83286a637577c5ba641a
Himanshu Anuragi <himanshu122122@gmail.com>
2020-02-09 22:57:09 +0530

    working on cardswiper

--- app/src/main/java/com/example/myapplication/cardswiper.java
+++ app/src/main/java/com/example/myapplication/cardswiper.java
@@ -1,21 +1,20 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class cardswiper extends AppCompatActivity {
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardswiper);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         
     }
