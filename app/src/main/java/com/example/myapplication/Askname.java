b12605ecfb57f61cbd73613ed3dea7ce35d855ba
Rishu-kmr <45550589+Rishu-kmr@users.noreply.github.com>
2020-02-21 00:56:20 +0530

    for swipecards

--- app/src/main/java/com/example/myapplication/Askname.java
+++ app/src/main/java/com/example/myapplication/Askname.java
@@ -25,7 +25,14 @@ public class Askname extends AppCompatActivity {
             public void onClick(View v) {//when next button is clicked it puts all the details entered into fields into public static valiables
                                         //which can be acessed by another activity outside this also we don't finish this activity as we will nedd this data while registering
                 Intent intent = new Intent(Askname.this,AskDOB.class);//creating a new intent pointing to AskEmail
+                String fullName = firstName.getText().toString()+" "+lastName.getText().toString();
+                intent.putExtra("fullName",fullName);   // by this u can use this in another activity and get it by getIntent().getExtras.getString("fullName");
+                // for multiple values u can use hashmap
+                // and pass it same as intent.putExtra("map",map)
+                // and getit by Intent intent = getIntent();
+                //    HashMap<String, String> data = (HashMap<String, String>)intent.getSerializableExtra("hash");
                 startActivity(intent);//starting this new intent
+
 
             }
         });
